2e439141fb21cfc14b7e6c914a7a207560543b5f
Alexander Evstratov <alexdrugev@me.com>
2024-08-28 01:20:52 +0300

    Add labirynth with plane.

---
+++ Assets/_Scipts/PlaneJoystickController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneJoystickController : MonoBehaviour
+{
+    [SerializeField] private FixedJoystick _fixedJoystick;
+    [SerializeField] private Transform _plane;
+    private float speed = 1f;
+
+    public void Update()
+    {
+        Vector3 direction = new Vector3(_fixedJoystick.Horizontal, _fixedJoystick.Vertical, 0);
+
+        direction = direction.normalized;
+
+        _plane.position += direction * speed * Time.deltaTime;
+
+        if (direction != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+
+            _plane.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+    }
+}
