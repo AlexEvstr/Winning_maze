91c6b5c361f600a304280141883340f9c00a7f97
Alexander Evstratov <alexdrugev@me.com>
2024-08-28 20:32:11 +0300

    Add 5 more labs, add rules.

--- Assets/_Scipts/GameController.cs
+++ Assets/_Scipts/GameController.cs
@@ -1,10 +1,29 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private GameObject _rules;
+    [SerializeField] private Button _rulesBtn;
+
     public void MenuButton()
     {
         SceneManager.LoadScene("Menu");
+    }
+
+    public void RulesButton()
+    {
+        StartCoroutine(ShowRules());
+    }
+
+    private IEnumerator ShowRules()
+    {
+        _rules.SetActive(true);
+        _rulesBtn.enabled = false;
+        yield return new WaitForSeconds(3.0f);
+        _rules.SetActive(false);
+        _rulesBtn.enabled = true;
     }
 }
