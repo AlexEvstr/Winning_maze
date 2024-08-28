dc4e1e8282c780fe1c20238a63f65993cf5dc237
Alexander Evstratov <alexdrugev@me.com>
2024-08-28 23:18:50 +0300

    Add sound and effects btns.

--- Assets/_Scipts/MenuController.cs
+++ Assets/_Scipts/MenuController.cs
@@ -5,6 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject _soundOn;
+    [SerializeField] private GameObject _soundOff;
+    [SerializeField] private GameObject _effectsOn;
+    [SerializeField] private GameObject _effectsOff;
+
+    private int _isEffects;
+
+    private void Start()
+    {
+        AudioListener.volume = PlayerPrefs.GetFloat("SoundStatus", 1);
+        if (AudioListener.volume == 1)
+        {
+            TurnOnSound();
+        }
+        else
+        {
+            TurnOffSound();
+        }
+
+        _isEffects = PlayerPrefs.GetInt("Effects", 1);
+        if (_isEffects == 1)
+        {
+            EnableEffects();
+        }
+        else
+        {
+            DisableEffects();
+        }
+    }
+
     public void PlayButton()
     {
         SceneManager.LoadScene("Game");
@@ -13,5 +43,37 @@ public class MenuController : MonoBehaviour
     public void OpenPrivacyPolicy()
     {
         Application.OpenURL("https://www.google.com/?client=safari");
+    }
+
+    public void TurnOffSound()
+    {
+        _soundOn.SetActive(false);
+        _soundOff.SetActive(true);
+        AudioListener.volume = 0;
+        PlayerPrefs.SetFloat("SoundStatus", 0);
+    }
+
+    public void TurnOnSound()
+    {
+        _soundOff.SetActive(false);
+        _soundOn.SetActive(true);
+        AudioListener.volume = 1;
+        PlayerPrefs.SetFloat("SoundStatus", 1);
+    }
+
+    public void DisableEffects()
+    {
+        _effectsOn.SetActive(false);
+        _effectsOff.SetActive(true);
+        _isEffects = 0;
+        PlayerPrefs.SetInt("Effects", _isEffects);
+    }
+
+    public void EnableEffects()
+    {
+        _effectsOff.SetActive(false);
+        _effectsOn.SetActive(true);
+        _isEffects = 1;
+        PlayerPrefs.SetInt("Effects", _isEffects);
     }
 }
